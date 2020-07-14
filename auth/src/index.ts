4a7a514b2212a1ba99f39961ed08ef88ac8d1be5
root <root@LAPTOP-M5RAVCSD.localdomain>
2020-07-14 14:35:43 -0300

    chore: Configurando o kubernetes de auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,15 @@ import {json} from 'body-parser'
 const app = express();
 app.use(json());
 
-app.listen(3000, () => {
+app.get('/api/users/currentuser', (req, res) => {
+    
+    console.log('E aí, Blz?');
+    res.send('Hi there!');
+ 
+});
 
-    console.log('Listening on port 3000');
+app.listen(3000, () => {
+    console.log('v0.0.1');
+    console.log('Listening on port 3000!');
     
 });
