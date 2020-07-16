cfe9cb199cbb496c41c7b8565add6fa4014e9ee3
root <root@LAPTOP-M5RAVCSD.localdomain>
2020-07-16 12:43:15 -0300

    chore: serviço para fazer o hash do password criado

---
+++ auth/src/models/user.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import { Password } from '../utils/password';
+
+//Interface que descreve os atributos necessários para se criar um usuário
+interface UserAttributes {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(attrs: UserAttributes): UserDocument;
+}
+
+interface UserDocument extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (done) {
+  console.log(this.get('password'));
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  console.log(this.get('password'));
+
+  done();
+});
+
+userSchema.statics.build = (userAttrs: UserAttributes) => {
+  return new User(userAttrs);
+};
+
+const User = mongoose.model<UserDocument, UserModel>('User', userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,32 +1,44 @@
-import express, {Request, Response} from 'express';
-import { body, validationResult} from 'express-validator';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connections-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-      .isEmail()
-      .withMessage('Email deve ser válido'),
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email deve ser válido'),
     body('password')
       .trim()
-      .isLength({ min: 4, max: 20})
-      .withMessage('Password deve ter entre 4 e 20 caracteres')
-],
-async (req: Request, res: Response) => {
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password deve ter entre 4 e 20 caracteres'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
-  if(!errors.isEmpty()){
-    throw new RequestValidationError(errors.array());
+    console.log('Criando usuário.');
+
+    const { email, password } = req.body;
+
+    const existinguser = await User.findOne({ email });
+
+    if (existinguser) {
+      console.log('Email em uso');
+      throw new BadRequestError('Email em uso');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send({ message: 'Done!', user });
+    console.log(`Usuario ${user.email} criado com sucesso`);
   }
-  
-  console.log('Criando usuário.')
-  throw new DatabaseConnectionError();
-  
-  res.send({ message: 'Done!'});
-
-});
+);
 
 export { router as signupRouter };
